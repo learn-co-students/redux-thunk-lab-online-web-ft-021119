71f846ff5cb284aba82e4812365aea069600dbd9
Lizzie <lchan217@gmail.com>
2019-07-15 09:24:46 -0500

    Done.

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux'
+import cats_reducer from './cats_reducer';
+
+ const rootReducer = combineReducers({
+    cats: cats_reducer
+})
+
+export default rootReducer;
